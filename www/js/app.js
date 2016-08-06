d211e4220cab6803058f12967f0f9bc18661f01a
tomiiwo <ftomiiwo@gmail.com>
2016-08-06 14:24:33 +0100

    localStorage Service injected, Ionic modal also was injected, controllers were created in app.js and several functions (tasks) were created

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,44 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+var app = angular.module('stodo', ['ionic', LocalStorageModule]);
+
+app.config(function(localStorageServiceProvider) {
+  localStorageServiceProvider.setPrefix('stodo');
+});
+
+ app.controller('main', function ($scope, $ionicModal, localStorageService) { 
+ //store the entities name in a variable var taskData = 'task';
+    //initialize the tasks scope with enpty array
+    $scope.tasks = [];
+
+    // initialize the task scope with empty object
+    $scope.task = {};
+
+    //configure the modal before use
+    $ionicModal.fromTemplateUrl('new-task-modal.html', {
+        scope: $scope, 
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.newTaskModal = modal;
+    });
+
+    $scope.getTasks = function() {
+
+        //This function will fetch task from local storage
+    }
+    $scope.createTask = function() {
+        // This function will create a new task
+
+    }
+    $scope.removeTask = function() {
+        // This function will remove a task
+    }
+    $scope.completeTask = function() {
+        //This function will update the state of a task
+    }
+
+ })
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
